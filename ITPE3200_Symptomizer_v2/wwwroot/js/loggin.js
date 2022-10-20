107c27a1483cbd5c559bc2fbeb42761a2dc649e0
Aleksander Korkh <korkh1979@gmail.com>
2022-10-20 13:56:26 +0200

    Created loggin.html and js.

---
+++ ITPE3200_Symptomizer_v2/wwwroot/js/loggin.js
@@ -0,0 +1,19 @@
+﻿function loggIn(){
+    const usernameOk = validateUserName();
+    const passwordOk = validatePassword();
+    if(usernameOk && passwordOk){
+        const user = {
+            username: $("#username").val(),
+            password: $("#password").val()
+        }
+        $.post("Patient/LoggIn", user, function (ok){
+            if(ok){
+                window.location.href = 'index.html';
+            }else{
+                $("#fail").html("Fail to logg in! Check user name or password!")
+            }
+        }).fail(function(){
+            $("#fail").html("Fail on server side - try again later!")
+        });
+    }
+}
